8d8ea1e1b7b3a70c3d26eadb8b318e8500b4bd3c
Quinn Bellmore <quinnzbellz@gmail.com>
2022-08-01 22:26:17 -0400

    making gud program

--- doomlauncher.py
+++ doomlauncher.py
@@ -1,19 +1,37 @@
+import sys
 import gi
 import os
-
-gi.require_version("Gtk", "4.0")
-
-from gi.repository import Gtk
-
-def on_activate(app):
-    win = Gtk.ApplicationWindow(application=app)
-    win = Gtk.Window(title="Linux Doom Launcher")
-    btn = Gtk.Button(label="Lift Off!")
-    btn.connect('clicked', lambda x: os.system("gzdoom"))
-    win.set_child(btn)
-    win.present()
+gi.require_version('Gtk', '4.0')
+gi.require_version('Adw', '1')
+from gi.repository import Gtk, Adw
 
 
-app = Gtk.Application(application_id='org.gtk.linuxdoomlauncher')
-app.connect('activate', on_activate)
-app.run(None)
+class MainWindow(Gtk.ApplicationWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.set_default_size(640, 480)
+        self.set_title("Linux Doom Launcher")
+        self.box1 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.box2 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.box3 = Gtk.Box(orientation=Gtk.Orientation.VERTICAL)
+        self.set_child(self.box1)
+        self.box1.append(self.box2) 
+        self.box1.append(self.box3)  
+        self.button = Gtk.Button(label="Lift Off!")
+        self.box1.append(self.button)
+        self.button.connect('clicked', self.gzdoom)
+    
+    def gzdoom(self, button):
+        os.system("gzdoom")
+
+class MyApp(Adw.Application):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.connect('activate', self.on_activate)
+
+    def on_activate(self, app):
+        self.win = MainWindow(application=app)
+        self.win.present()
+
+app = MyApp(application_id="org.gtk.linuxdoomlauncher")
+app.run(sys.argv)
