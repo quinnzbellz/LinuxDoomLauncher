f9c20ccb3b7d35c3367caf141c19adde2056c00b
Quinn Bellmore <quinnzbellz@gmail.com>
2022-08-02 09:23:59 -0400

    adding mod loading functionality

--- doomlauncher.py
+++ doomlauncher.py
@@ -17,12 +17,29 @@ class MainWindow(Gtk.ApplicationWindow):
         self.set_child(self.box1)
         self.box1.append(self.box2) 
         self.box1.append(self.box3)  
+        self.open_button = Gtk.Button(label="Open PK3 #1")
+        self.box3.append(self.open_button)
+        self.open_dialog = Gtk.FileChooserNative.new(title="Choose a PK3", 
+                                                     parent=self, action=Gtk.FileChooserAction.OPEN)
+        
+        self.open_dialog.connect("response", self.open_response)
+        self.open_button.connect("clicked", self.show_open_dialog)
+
+
         self.button = Gtk.Button(label="Lift Off!")
         self.box2.append(self.button)
         self.button.connect('clicked', self.gzdoom)
+
+    def show_open_dialog(self, button):
+        self.open_dialog.show()
+    
+    def open_response(self, dialog, response):
+        if response == Gtk.ResponseType.ACCEPT:
+            file = dialog.get_file()
+            filename = file.get_path()
     
     def gzdoom(self, button):
-        os.system("gzdoom")
+        os.system("gzdoom -file".format(filename))
 
 class MyApp(Adw.Application):
     def __init__(self, **kwargs):
